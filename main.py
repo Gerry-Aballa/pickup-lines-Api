834a44b8458c97c6121368f2622a6f4e8897736a
Gerry_Aballa <gerry.o.aballa@gmail.com>
2023-09-21 07:46:38 +0300

    Edited main.py

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ def random_line(db: Session = Depends(db_connection)):
     return random_line
 
 
-if __name__ == "__main__":
-   import uvicorn
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
